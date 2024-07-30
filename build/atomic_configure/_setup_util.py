6f752a3d1d81099cfd4312f3676ec41e7ff79cf5
120090049 <120090049@link.cuhk.edu.cn>
2024-07-30 09:59:11 +0800

    ...

--- build/atomic_configure/_setup_util.py
+++ build/atomic_configure/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/clp/ros2ws/install/turtlesim'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/clp/catkin_ws/devel;/opt/ros/noetic;/home/clp/ros2ws/install/turtlesim'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/clp/catkin_ws/src/lias_ocean_sim/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/clp/catkin_ws/src/lias_ocean_sim/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/clp/catkin_ws/src/lias_ocean_sim/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/clp/catkin_ws/src/lias_ocean_sim/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/clp/catkin_ws/src/lias_ocean_sim/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/clp/catkin_ws/src/lias_ocean_sim:$ROS_PACKAGE_PATH"
