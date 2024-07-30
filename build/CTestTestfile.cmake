1d50c0de1c4ae39aedcc457733048a03a0e1c35a
120090049 <120090049@link.cuhk.edu.cn>
2024-07-30 09:59:38 +0800

    ...

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/clp/catkin_ws/src/lias_ocean_sim
-# Build directory: /home/clp/catkin_ws/src/lias_ocean_sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
